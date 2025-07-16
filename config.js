eab54082cad6bfa2a6369efdda41eaecd5a9d9e9
Gagananilupul <gagananilupul2749@gmail.com>
2025-07-16 18:53:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'igwBmZLI#3H3JNwxR20MexNQPGEABEuB-iVyH_0SSU7X4s7AOcsY' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VISPER-MD&Th4w0QLI#D0sAMROangyUD2PdbRK7Jt2T6VebOSN6P8V8_rxgsu0' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'safari(mac os)' : process.env.SESSION_NAME, 
 
